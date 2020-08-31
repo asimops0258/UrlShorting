95bddce62ec428bac34a8cb472a50cfc5dbe1f5a
Dewei Li <asimops0258@gmail.com>
2020-08-31 23:30:39 +0800

    Update ban.php

--- admin/ban.php
+++ admin/ban.php
@@ -9,16 +9,16 @@
   $content=$arr['content'];
   if (empty($content))
   {
-    echo("<center><h2>暂时没有更多信息</h2></center>");
+    echo("<center><h2>暫時沒有封鎖的內容</h2></center>");
     require_once("../footer.php");
     exit();
   }else{
-    echo "已被ban列表:<br /><br /><center><div class=\"mdui-table-fluid\">
+    echo "封鎖列表:<br /><br /><center><div class=\"mdui-table-fluid\">
                         <table class=\"mdui-table mdui-table-hoverable\">
                             <tr>
-                                <th>种类</th>
-                                <th>IP或短域</th>
-                                <th>状态</th>
+                                <th>種類</th>
+                                <th>IP或短網址</th>
+                                <th>狀態</th>
                             </tr>";
   }
   $comd = "SELECT * FROM `ban` order by time DESC";
@@ -29,7 +29,7 @@
         <td>$row->type</td>
         <td>$row->content</td>
               <td>
-              <a href=\"./processing.php?content=$row->content&&type=cancel&&from=ban\" class=\"mdui-btn mdui-btn-raised mdui-ripple\">解除</a>
+              <a href=\"./processing.php?content=$row->content&&type=cancel&&from=ban\" class=\"mdui-btn mdui-btn-raised mdui-ripple\">解除封鎖</a>
               </td>
 
       </tr>");
