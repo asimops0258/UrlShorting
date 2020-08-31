60f0a54334b561e8580f8bbdbb7a118b3d87aa65
Dewei Li <asimops0258@gmail.com>
2020-08-31 21:47:52 +0800

    Update help.php

--- help.php
+++ help.php
@@ -1,13 +1,13 @@
 <?php require_once "header.php"; ?>
 <div class="mdui-container doc-container">
     <div class="mdui-typo">
-        <h2>帮助</h2>
-        1.输入短域请加上http(s)://<br />
-        2.中文域名请手动Punycode编码后再使用<br />
-        3.网址最长支持1000字符<br />
-        4.密语最长支持3000字符(合1000汉字)<br />
-        5.手动填写短域以及密码为可选项目<br />
-        6.密码限制2-20位(数字密码组合)/短域限制输入<?php echo $pass ?>位<br />
+        <h2>幫助</h2>
+        1.輸入短網址時請加上http(s)://<br />
+        2.中文網址請使用Punycode編碼後再使用<br />
+        3.網址最長支援1000字符<br />
+        4.密語最長支援3000字符(大約1000漢字)<br />
+        5.自訂短網址 Or 密码(選填)<br />
+        6.密碼限制2-20位(英數組合)/短網址限制輸入<?php echo $pass ?>位<br />
     </div>
 </div>
 <div class="mdui-container doc-container">
@@ -23,7 +23,7 @@
                     </tr>
                     <tr>
                         <td>注意</td>
-                        <td>请使用post来访问Api</td>
+                        <td>請使用post來訪問Api</td>
                     </tr>
                 </tbody>
             </table>
@@ -32,26 +32,26 @@
             <table class="mdui-table mdui-table-hoverable">
                 <thead>
                     <tr>
-                        <th>参数名</th>
-                        <th>含义</th>
+                        <th>變數名稱</th>
+                        <th>含義</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>url</td>
-                        <td>需要缩短的网址(优先于密语)</td>
+                        <td>需要縮址的網址(優先於密語)</td>
                     </tr>
                     <tr>
                         <td>message</td>
-                        <td>需要缩短的密语</td>
+                        <td>需要縮短的密語</td>
                     </tr>
                     <tr>
                         <td>shorturl</td>
-                        <td>自定义短域(可选)</td>
+                        <td>自定短網址(選填)</td>
                     </tr>
                     <tr>
                         <td>passwd</td>
-                        <td>加密密码(可选)</td>
+                        <td>加密密碼(選填)</td>
                     </tr>
                 </tbody>
             </table>
@@ -61,30 +61,30 @@
                 <thead>
                     <tr>
                         <th>返回值(json)</th>
-                        <th>含义</th>
+                        <th>含義</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>code</td>
-                        <td>状态码,见下表</td>
+                        <td>狀態碼,參照下表</td>
                     </tr>
                     <tr>
                         <td>shorturl</td>
-                        <td>生成的短网址,只有在code为200时才会返回</td>
+                        <td>生成的短網址,只有在code為200時才會返回</td>
                     </tr>
                 </tbody>
             </table>
         </div>
         <div class="mdui-container doc-container">
             <div class="mdui-typo">
-                <h2>状态码释义</h2>
+                <h2>狀態碼表</h2>
                 <div class="mdui-table-fluid">
                     <table class="mdui-table mdui-table-hoverable">
                         <thead>
                             <tr>
-                                <th>状态码</th>
-                                <th>含义</th>
+                                <th>狀態碼</th>
+                                <th>含義</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -94,23 +94,23 @@
                             </tr>
                             <tr>
                                 <td>1002</td>
-                                <td>您的IP或短域被封禁</td>
+                                <td>您的IP或短網址被管理員封鎖</td>
                             </tr>
                             <tr>
                                 <td>1001</td>
-                                <td>非法的输入</td>
+                                <td>未經授權的輸入</td>
                             </tr>
                             <tr>
                                 <td>2001/2002</td>
-                                <td>非法的自定义短域</td>
+                                <td>不合法的自定義短網址</td>
                             </tr>
                             <tr>
                                 <td>2003</td>
-                                <td>自定义短域已被使用</td>
+                                <td>自訂義名稱已被使用</td>
                             </tr>
                             <tr>
                                 <td>3001/3002</td>
-                                <td>非法的密码</td>
+                                <td>未經授權的密碼</td>
                             </tr>
                         </tbody>
                     </table>
